e051955fcc32ad7ed247a44619b6407bd749968d
Emmanuel Shutsha <Emmanuel.Shutsha@nology.io>
2018-12-17 12:22:58 +0000

    login register started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mock-brief';
+  items: Observable<any[]>;
+  constructor(afs: AngularFirestore) {
+    this.items = afs.collection('items').valueChanges();
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDHrTFFIrVJsfwUmIuHAqXhOUsX2yl1ZGk",
+    authDomain: "nology-course.firebaseapp.com",
+    databaseURL: "https://nology-course.firebaseio.com",
+    projectId: "nology-course",
+    storageBucket: "nology-course.appspot.com",
+    messagingSenderId: "458087984894"
+  }
 };
